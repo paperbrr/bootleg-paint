1df6aed2d609d7a63529afe7938ca5217d2d3971
PaperBrr <120294723+paperbrr@users.noreply.github.com>
2024-01-27 23:35:15 +0530

    Modified color values, added default color to allow changes in brush, added key-mapping for different colors

--- EventHandler.c
+++ EventHandler.c
@@ -3,16 +3,18 @@
 #include "Decs.h"
 #include <math.h>
 
-SDL_Color black = {0, 0, 0};
-SDL_Color white = {255, 255, 255};
-SDL_Color red = {200, 0, 0};
-SDL_Color blue = {0, 200, 0};
-SDL_Color green = {0, 0, 200};
-SDL_Color yellow = {220, 220, 0};
-SDL_Color purple = {148, 0, 211};
-SDL_Color orange = {100, 64, 0};
-SDL_Color lavender = {90, 90, 98};
-SDL_Color grey = {152, 115, 172};
+SDL_Color black = {0, 0, 0, 255};
+SDL_Color white = {255, 255, 255, 255};
+SDL_Color red = {200, 0, 0, 255};
+SDL_Color blue = {0, 0, 200, 255};
+SDL_Color green = {0, 200, 0, 255};
+SDL_Color yellow = {220, 220, 0, 255};
+SDL_Color purple = {148, 0, 211, 255};
+SDL_Color orange = {255, 165, 0, 200};
+SDL_Color peach = {255, 218, 185, 255};
+SDL_Color grey = {90, 90, 98, 255};
+
+SDL_Color renderColor;
 
 void snapToGrid(int* mouseX, int* mouseY);
 
@@ -25,11 +27,23 @@ void eventHandler(int* gameLoop, SDL_Event* event, Workspace* workspace){
         snapToGrid(&xPos, &yPos);
         if (event->button.button == SDL_BUTTON_LEFT) {
             SDL_Color color = {100, 150, 200, 255};
-            createBlock(workspace, xPos, yPos, 50, 50, lavender);
+            createBlock(workspace, xPos, yPos, 50, 50, renderColor);
         }
         else{
             deleteBlock(workspace, xPos, yPos);
         }
+    }
+    else if (event->type == SDL_KEYDOWN){
+        int keycode = event->key.keysym.sym;
+        if (keycode==SDLK_1){renderColor=black;}
+        else if (keycode==SDLK_2){renderColor=red;}
+        else if (keycode==SDLK_3){renderColor=green;}
+        else if (keycode==SDLK_4){renderColor=blue;}
+        else if (keycode==SDLK_5){renderColor=yellow;}
+        else if (keycode==SDLK_6){renderColor=orange;}
+        else if (keycode==SDLK_7){renderColor=purple;}
+        else if (keycode==SDLK_8){renderColor=grey;}
+        else if (keycode==SDLK_9){renderColor=peach;}
     }
 }
 
