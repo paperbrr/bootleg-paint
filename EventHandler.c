2f6191274e10ecb93958da61052ce4941463c56c
PaperBrr <120294723+paperbrr@users.noreply.github.com>
2024-01-27 23:18:16 +0530

    added color values

--- EventHandler.c
+++ EventHandler.c
@@ -3,6 +3,17 @@
 #include "Decs.h"
 #include <math.h>
 
+SDL_Color black = {0, 0, 0};
+SDL_Color white = {255, 255, 255};
+SDL_Color red = {200, 0, 0};
+SDL_Color blue = {0, 200, 0};
+SDL_Color green = {0, 0, 200};
+SDL_Color yellow = {220, 220, 0};
+SDL_Color purple = {148, 0, 211};
+SDL_Color orange = {100, 64, 0};
+SDL_Color lavender = {90, 90, 98};
+SDL_Color grey = {152, 115, 172};
+
 void snapToGrid(int* mouseX, int* mouseY);
 
 void eventHandler(int* gameLoop, SDL_Event* event, Workspace* workspace){
@@ -14,7 +25,7 @@ void eventHandler(int* gameLoop, SDL_Event* event, Workspace* workspace){
         snapToGrid(&xPos, &yPos);
         if (event->button.button == SDL_BUTTON_LEFT) {
             SDL_Color color = {100, 150, 200, 255};
-            createBlock(workspace, xPos, yPos, 50, 50, color);
+            createBlock(workspace, xPos, yPos, 50, 50, lavender);
         }
         else{
             deleteBlock(workspace, xPos, yPos);
