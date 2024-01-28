1a67bd113bfa26682a86f759fc0f7552159bb47f
PaperBrr <120294723+paperbrr@users.noreply.github.com>
2024-01-28 23:03:19 +0530

    added button declarations

--- Decs.h
+++ Decs.h
@@ -3,6 +3,18 @@
 
 typedef struct{
     SDL_Rect sourceRect;
+    SDL_Color color;
+    void (*actionFunc) ();
+} Button;
+
+typedef struct{
+    int length;
+    int arrDataSize;
+    Button** buttons;
+} ButtonsArray;
+
+typedef struct{
+    SDL_Rect sourceRect;
     SDL_Texture* blockTexture;
 	SDL_Color blockColor;
 } Block;
@@ -32,3 +44,9 @@ void setRenderColor(SDL_Color color, SDL_Renderer* renderer);
 
 
 void renderNext(Workspace* workspace);
+
+void buttonsArr_init(ButtonsArray* buttonArr, int initialSize);
+void createButton(ButtonsArray* buttonArr, int x, int y, int h, int w, void (*actionPtr) (), SDL_Color color);
+void renderButtons(ButtonsArray* buttonArr, SDL_Renderer* renderer);
+void buttonClickHandler(SDL_Event* event ,ButtonsArray* buttonArr);
+void freeButtonArray(ButtonsArray* buttonArr);
