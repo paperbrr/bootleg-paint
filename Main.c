1df395e8c4dcfd301c5baa3f87deb3627eece8b9
PaperBrr <120294723+paperbrr@users.noreply.github.com>
2024-01-29 00:21:09 +0530

    created panel and canvas frames

--- Main.c
+++ Main.c
@@ -11,13 +11,22 @@ int main(int argv, char* args[]){
 	//SDL_Surface* oikawaSurface;
 
 	SDL_Init(SDL_INIT_VIDEO);
-	window = SDL_CreateWindow("BOOTLEG PAINT", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 480, 0);
+	window = SDL_CreateWindow("BOOTLEG PAINT", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
 	Workspace workspace;
 
 	workspace.renderer = renderer;
 	blockArrayInit(&workspace.blocksArr, 1);
+	buttonsArr_init(&workspace.buttonsArr, 1);
+	frameArr_init(&workspace.framesArr, 1);
+
+	SDL_Color panelColor = {150,150,150,255};
+	SDL_Color canvasColor = {240,240,240,255};
+	createFrame(&workspace.framesArr, 0, 0, 800, 100, panelColor);
+	createFrame(&workspace.framesArr, 0, 100, 800, 700, canvasColor);
+
+	//createAllButtons(&workspace.buttonsArr);
 
 	int gameLoop = 1;
 	SDL_Event event;
@@ -25,6 +34,7 @@ int main(int argv, char* args[]){
 	while (gameLoop) {
 		while (SDL_PollEvent(&event)!=0) {
 			eventHandler(&gameLoop, &event, &workspace);
+			buttonClickHandler(&event, &workspace.buttonsArr);
 		}
 
 		renderNext(&workspace);
@@ -34,6 +44,8 @@ int main(int argv, char* args[]){
 	//SDL_DestroyTexture(workspace.oikawaTexture);
 	//SDL_DestroyTexture(workspace.block1.blockTexture);
 	freeBlockArray(&workspace.blocksArr);
+	freeButtonArray(&workspace.buttonsArr);
+	freeFramesArray(&workspace.framesArr);
 
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);
