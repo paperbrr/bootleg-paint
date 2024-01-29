9a5b1b6c7235006b890bb2189ee92e6df31a1990
PaperBrr <120294723+paperbrr@users.noreply.github.com>
2024-01-30 01:30:39 +0530

    Delete ColorHandler.c copy

--- ColorHandler.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "Decs.h"
-#include <SDL.h>
-
-void setRenderColor(SDL_Color color, SDL_Renderer* renderer){
-    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
-}
